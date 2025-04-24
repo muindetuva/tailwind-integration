de95cf5788275690c205f5cd59e1f115b082f683
Alfred Tuva <muindetuva@gmail.com>
2025-04-24 11:57:51 +0300

    add interactivity

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,12 +1,14 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-36 h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
       <p className="text-gray-600 text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
